ab5e0593828327e2c4e4a9942b8badebdabab1e4
piyush rathore <piysuh260305@gmail.com>
2024-09-18 19:13:14 +0530

    uploading files

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,63 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const registeredUser = asyncHandler(async (req, res) => {
+  //getting details from Frontend
+  const { fullName, email, password, username, avatar, coverImage } = req.body;
+
+  //checks -if any of the field is empty
+  if (!fullName && !email && !password && !username && !avatar) {
+    throw new ApiError(400, "all field are required");
+  }
+
+  // check if the user already exists
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (existedUser) {
+    throw new ApiError(409, "user already exists");
+  }
+
+  // check for avatar file
+  console.log(req.files);
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  console.log(avatarLocalPath);
+
+  //upload on cloudinary
+  const UploadedAvatar = await uploadOnCloudinary(avatarLocalPath);
+  const uploadedCoverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(409, "user already exists");
+  }
+
+  //creating user once the image is uploaded
+  const user = await User.create({
+    fullName,
+    username,
+    password,
+    email,
+    avatar,
+    coverImage: coverImage?.url || "",
+  });
+
+  //removing password and refresh token from response
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  //checking for response
+  if (!createdUser) {
+    throw new ApiError(409, "something went wrong while registering the user ");
+  }
+  
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "user registered successfully"));
+});
+
+export { registeredUser };
---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+
+// Configure multer storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/temp");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+
+
+
+
+export { registeredUser };

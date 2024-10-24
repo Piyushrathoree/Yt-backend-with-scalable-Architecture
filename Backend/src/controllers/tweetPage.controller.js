ccacb55619e3a6bb57ffae8f21902195f42665f0
piyush rathore <piysuh260305@gmail.com>
2024-10-24 16:26:22 +0530

    controllers for home page for videos and tweets

---
+++ Backend/src/controllers/tweetPage.controller.js
@@ -0,0 +1,1 @@
+//made a tweet page wehere all the videos were shown randomely with search tab and the whole navbar (with subscriptions , profile )
---
+++ Backend/src/controllers/videoPage.controller.js
@@ -0,0 +1,1 @@
+//made a video page wehere all the videos were shown randomely with search tab and the whole navbar (with subscriptions , playlists , profile )
